d46c9e85758e7a8dbca0bc03bafdc38cc5d3c04d
Mosen <m.mohsin22055@gmail.com>
2024-12-06 00:51:01 +0500

    Dashboard updated

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,16 +1,25 @@
-import { AppSidebar } from '@/components/Sidebar/app-sidebar'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import Sidebar from '@/components/Sidebar'
+import { ClerkProvider } from '@clerk/nextjs'
+
 import React from 'react'
 
 
 export default function layout({children}) {
   return (
-    <SidebarProvider>
-    <AppSidebar />
-    <main>
-      <SidebarTrigger />
-      {children}
-    </main>
-  </SidebarProvider>
+   <ClerkProvider>
+
+   
+    <div>
+      <div className="md:w-64 fixed ">
+        <Sidebar />
+      </div>
+      <div className="md:ml-64 ">
+        {/* <Header /> */}
+        {children}
+      </div>
+    </div>
+    </ClerkProvider>
+
+  
   )
 }
---
+++ components/Sidebar.jsx
@@ -0,0 +1,75 @@
+'use client'
+import Link from 'next/link';
+import React from 'react'
+import { Button } from './ui/button';
+import {  FaBriefcase, FaLaptop, FaShieldAlt } from "react-icons/fa";
+import { BsLayoutTextSidebarReverse } from "react-icons/bs";
+import { usePathname } from 'next/navigation';
+import { BookTemplate, CircleHelp, Settings, Settings2 } from 'lucide-react';
+import { UserButton, useUser } from '@clerk/nextjs';
+
+
+const Sidebar = () => {
+    const path  = usePathname();
+
+    const user  = useUser();
+
+        
+      return (
+        <div className="shadow-md h-screen p-7" >
+        {/* <Link href={"/"}> <Image src="./logo.svg" width={120} height={80} alt="Logo"/></Link> */}
+        <h1 className='text-3xl font-black font-sans text-start px-2'>ResumX</h1>
+   
+         <div className=" flex flex-col  gap-2 mt-6" >   
+           {/* <PdfDialog isMax={files?.length>=5?true:false}/> */}
+
+          <Link href={`/dashboard`}>
+          
+          <Button variant="ghost" className={`w-full flex justify-start ${path == "/dashboard"&&"bg-slate-100"}`}>
+             <BsLayoutTextSidebarReverse className=""/>  Dashboard
+           </Button></Link>
+          <Link href={`/dashboard/templates`}>
+          
+          <Button variant="ghost" className={`w-full flex justify-start ${path == "/dashboard/templates"&&"bg-slate-100"}`}>
+             <BookTemplate className=""/>  Templates
+           </Button></Link>
+          <Link href={'/dashboard/jobs'}>
+          <Button variant="ghost"className={`w-full flex justify-start ${path == "/dashboard/jobs"&&"bg-slate-100"}`}>
+              <FaBriefcase/> Jobs 
+           </Button>
+          </Link>
+          <Link href={'/dashboard/job-appliactions'}>
+          <Button variant="ghost"className={`w-full flex justify-start ${path == "/dashboard/job-appliactions"&&"bg-slate-100"}`}>
+              <FaLaptop/> Job Applications 
+           </Button>
+          </Link>
+         </div>
+       <div className="absolute bottom-20 w-[75%] border-t border-slate-300 pt-4">
+       <Link href={'/dashboard/job-appliactions'}>
+          <Button variant="ghost"className={`w-full flex justify-start ${path == "/dashboard/job-appliactions"&&"bg-slate-100"}`}>
+              <Settings/>Settings
+           </Button>
+          </Link>
+          <Link href={'/dashboard/job-appliactions'}>
+          <Button variant="ghost"className={`w-full flex justify-start ${path == "/dashboard/job-appliactions"&&"bg-slate-100"}`}>
+              <CircleHelp/> Help & Support
+           </Button>
+          </Link>
+            <div className=' flex flex-row p-4 gap-2'>
+                <UserButton/>
+                <div className=''>
+                    <h1 className='text-md text-sm '>{user.user?.fullName}</h1>
+                    <p className='text-xs text-gray-400 font-light'>{user.user?.primaryEmailAddress?.emailAddress}</p>
+                </div>
+            </div>
+            <Link href={'/dashboard/upgrade'}>
+          <Button className={`w-full flex justify-start ${path == "/dashboard/upgrade"&&"bg-slate-100"}`}>
+              <FaShieldAlt/> Upgrade to Premium
+           </Button>
+          </Link>
+       </div>
+       </div>
+      )
+}
+
+export default Sidebar
